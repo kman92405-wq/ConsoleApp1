aba237f99d57182599928e984b050ffcadf8748b
Vova <kman92405@gmail.com>
2025-09-25 10:52:59 +0300

    Добавлены комментарии и подпись автора программы.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main()
     {
+        // Запрос числа у пользователя
         Console.WriteLine("Введите число:");
         string input = Console.ReadLine();
         double x;
 
+        // Проверка на корректность ввода
         bool rez = double.TryParse(input, out x);
         if (rez == false)
         {
@@ -15,10 +17,16 @@ class Program
             return;
         }
 
+        // Вычисление значения функции 3 * sin(x + 2)
         double final = 3 * Math.Sin(x + 2);
         Console.WriteLine($"Результат: {final}");
+
+        // Подпись автора
+        Console.WriteLine("Работу выполнил Сорокин Владимир, Цуканов Федор 23ИС");
+
         Console.ReadKey();
     }
 }
+
 
 
